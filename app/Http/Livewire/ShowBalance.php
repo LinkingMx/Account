143c2f79a84fbb6ffcb3b8b0b5350ace9076c723
LinkingMx <armando.reyes@live.com.mx>
2023-02-14 11:07:20 -0600

    Next changes

--- app/Http/Livewire/ShowBalance.php
+++ app/Http/Livewire/ShowBalance.php
@@ -4,17 +4,33 @@ namespace App\Http\Livewire;
 
 use App\Models\Transactions;
 use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
 
 class ShowBalance extends Component
 {
     public $balance; 
+    public $last_update;
+    public $rows = 5;
+    public $total_rows;
 
     public function render()
     {
 
-        $transactions = Transactions::where('user_id', 1)->get();
+        $transactions = Transactions::where('user_id', Auth::user()->id)
+        ->orderBy('date', 'DESC')
+        ->get();
+
         $this->balance = $transactions->sum('amount');
+        $this->total_rows = $transactions->count();
+        $this->last_update = $transactions->take(1);
 
         return view('livewire.show-balance', compact('transactions'));
+    }
+
+    /**
+     * Function to increment rows of de pagination or load more
+     */
+    public function load(){
+        $this->rows += 5;
     }
 }
--- resources/views/livewire/show-balance.blade.php
+++ resources/views/livewire/show-balance.blade.php
@@ -1,31 +1,36 @@
 <div class="m-2">
     {{-- In work, do what you enjoy. --}}
-    
-
-    <a href="#" class="mb-2 text-center block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+    <div class="mb-2 text-center block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
         <p class="mb-5 tracking-tight text-gray-900 dark:text-white">Welcome: {{ Auth::user()->name }}</p>
         <p class="text-2xl font-bold text-gray-700 dark:text-gray-400">${{ number_format($balance,2,'.',','); }}</p>
-        <small>last balance update: {{ date("Y-m-d") }}</small>
-    </a>
-
-    <a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-        <h5 class="mb-6">Ultimos movimientos</h5>
+        <small>last balance update: {{ $last_update[0]->date }}</small>
+    </div>
+    <!--
+    Total Rows = {{ $total_rows;}}
+    Rows = {{ $rows }}
+    -->
+    <div class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow  dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+        <h5 class="mb-6 text-[#6875F5]">last transactions (5)</h5>
 
         <div class="relative overflow-x-auto">
             <table class="table-fixed">
                 <tbody>
-                    @foreach ($transactions as $item)
+                    @foreach ($transactions->take($rows) as $item)
                     <tr class="pt-4 mt-4 space-y-2 border-b border-gray-200 dark:border-gray-700">
                         <td class="@if( $item->amount < 0 ) text-sky-500 @else text-green-500 @endif flex p-3 text-sm">
                             <svg class="w-4 h-4 mr-1.5 @if( $item->amount < 0 ) text-sky-500 @else text-green-500 @endif dark:text-green-400 flex-shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
-                            {{ ucfirst( $item->subject ) }}</td>
-                        <td class="align-top w-1/3 text-sm pt-4">${{ number_format($item->amount,2,'.',','); }}</td>
+                            <div class="capitalize">{{ ucfirst( $item->subject ) }}</div>
+                        </td>
+                        <td class="align-top w-1/3 text-sm pt-4 pb-2">
+                            ${{ number_format($item->amount,2,'.',','); }}
+                            <small class="text-gray-500 text-xs italic">{{ $item->date; }}</small>
+                        </td>
                     </tr>
                     @endforeach
                 </tbody>
               </table> 
         </div>
-    </a>
+        <button wire:click="load" type="button" @if ( $total_rows < $rows ) disabled @endif class="mt-5 px-3 py-2 font-medium text-center text-white bg-[#6875F5] rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">@if ( $total_rows < $rows ) No more rows...@else Load more... @endif</button>
 
-
+    </div>
 </div>
